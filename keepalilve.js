ef6e70b00288b478a81d388c62d52fec75d8d068
uumeigr <jade.android.2011@gmail.com>
2026-01-10 10:50:46 +0800

    Update keepalilve.js

--- keepalilve.js
+++ keepalilve.js
@@ -1,25 +1,25 @@
 // This script runs in a Node.js environment (like GitHub Actions)
 //const fetch = require('node-fetch');
+import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
+// Create a single supabase client for interacting with your database
+const supabase = createClient('SUPABASE_URL', 'SUPABASE_ANON_KEY')
+
+
+
+
 // Pings an endpoint to keep the project active
 async function keepAlive() {
   try {
-    // Replace 'your_table_name' with the name of your small keep-alive table
-    const response = await fetch(`${SUPABASE_URL}/rest/v1/keep_alive_ping`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'apikey': SUPABASE_ANON_KEY,
-      },
-    });
+    const { data, error } = await supabase.from('keep_alive_ping').select();
 
-    if (response.ok) {
+    if (data[status] == 200) {
       console.log('Supabase project ping successful. Project is awake.');
     } else {
-      console.error('Supabase project ping failed. Status:', response.status);
+      console.error('Supabase project ping failed. Status:', data.[status]);
     }
   } catch (error) {
     console.error('An error occurred during Supabase keep-alive ping:', error);
