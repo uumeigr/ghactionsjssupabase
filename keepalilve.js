d15bff0c31a239b6b7bde17bd0c8ba60ffcbfb30
uumeigr <jade.android.2011@gmail.com>
2026-01-11 17:33:18 +0800

    Update keepalilve.js

--- keepalilve.js
+++ keepalilve.js
@@ -4,14 +4,22 @@ import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+console.log("The supabase url is: " + SUPABASE_URL);
+console.log("The supabase anon key is: " + SUPABASE_ANON_KEY);
 
-console.log(SUPABASE_URL);
-console.log(SUPABASE_ANON_KEY);
+console.log("Breakpoint 1 ----------------------------------------------------------------------- set by J10");
+
+const SUPABASE_URL_FR_SECRETS = ${{ secrets.SUPABASE_URL }};
+const SUPABASE_ANON_KEY_FR_SECRETS = ${{ secrets.SUPABASE_ANON_KEY }};
+console.log("The supabase url is: " + SUPABASE_URL_FR_SECRETS);
+console.log("The supabase anon key is: " + SUPABASE_ANON_KEY_FR_SECRETS);
+
+console.log("Breakpoint 2 ----------------------------------------------------------------------- set by J10");
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
-
+console.log("Breakpoint 3 ----------------------------------------------------------------------- set by J10");
 
 
 // Pings an endpoint to keep the project active
@@ -28,11 +36,16 @@ async function keepAlive() {
       const currentDate = new Date();
       const timestamptz = currentDate.toISOString();
 
+      console.log(timestamptz);
+    
+    
+
       const { error } = await supabase
         .from('keep_alive_ping')
         .insert({ id: 2, last_ping: timestamptz });
-    
-      console.log(timestamptz);
+
+      console.log("Breakpoint 4 ----------------------------------------------------------------------- set by J10");
+      
     
   } catch (error) {
     console.error('An error occurred during Supabase keep-alive ping:', error);
