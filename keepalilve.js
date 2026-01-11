3ff20f28b2ee4133e8bb7870529a244713d94405
uumeigr <jade.android.2011@gmail.com>
2026-01-11 17:21:18 +0800

    Update keepalilve.js

--- keepalilve.js
+++ keepalilve.js
@@ -6,7 +6,7 @@ const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('SUPABASE_URL', 'SUPABASE_ANON_KEY')
+const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 
 
 
