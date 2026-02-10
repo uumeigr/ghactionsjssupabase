d11400c53c3e6f6a998d2d7ad1a2791995e9ebed
uumeigr <jade.android.2011@gmail.com>
2026-02-10 12:51:53 +0800

    Update keepalilve.js

--- keepalilve.js
+++ keepalilve.js
@@ -24,8 +24,12 @@ console.log("Breakpoint -------------------------------------002----------------
 // Create a single supabase client for interacting with your database
 const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY)
 const currentDate = new Date();
-const timestamptz = currentDate.toISOString();
+const zulutime = currentDate.toISOString();
+timestamptz = zulutime.split("\.")[0]+"+00:00";
 console.log(timestamptz);
+//const zulutime = "2026-02-10T04:30:45.516Z";
+//const timestamp = zulutime.split("\.")[0]+"+00:00";
+//console.log(timestamp);
 
 console.log("Breakpoint -------------------------------------003------------------------------- set by J10");
 
@@ -38,6 +42,7 @@ async function keepAlive() {
     console.log(resp);
     console.log("new record No. is --- " + resp.data.length + 1);
     console.log("new record Time Stamp is --- " + timestamptz);
+    console.log(resp.data);
     await supabase.from('keep_alive_ping').insert([{ id: resp.data.length + 1, last_ping: timestamptz }]);
     
     //Check the timestamp, select statement and from node in Supabase dashboard /left bar/ Logs, the query is exectued successfully
